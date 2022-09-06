0f2559326de3b831ee9a72b77fa57b10e9792c49
ReyvanMacross <88920121+ReyvanMacross@users.noreply.github.com>
2022-09-06 21:38:31 +0700

    first commit

---
+++ app/src/main/java/com/example/fragmentsederhana/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.fragmentsederhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_fragment = (Button) findViewById(R.id.btn_fragment);
+        btn_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
